1d555419f5b2941d6b7c31d654f1c5c94cdc670b
MiDFNGR <33682180+MiDFNGR@users.noreply.github.com>
2017-11-15 01:14:59 -0700

    Create checkstake.sh
    This bash script sends email notifications when QTUM wallet "stake" value changes.  It sends both a short email-to-text and a full email with more info.  It will also notify if the QTUM wallet is not "unlocked", which can happen after a reboot.  
    
    When a stake change is detected, the script waits until 6 confirmations (blocks) have been added on top of the staked block before logging the "stake change" and sending out the notifications.
    
    If the stake value reverts to the previous value before 6 confirmations, the script logs it as a "stake orphan" and sends no notification.

---
+++ checkstake.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+#
+# Configuration Variables
+#
+SENDER=youraddress@gmail.com
+NOTIFYRECIPIENT=5558671234@tmomail.net
+INFORECIPIENT=youraddress@gmail.com
+HOMEDIR=/home/qtum
+#
+# Static Variables
+#
+SUBJECT="QTUM Stake Change"
+QTUMCLI=$HOMEDIR/qtum-wallet/bin/qtum-cli
+SCRIPTDIR=qt-notify
+SCRIPTNAME=$(basename -- "$0")
+PIDDIR=$HOMEDIR/$SCRIPTDIR/run
+LOGDIR=$HOMEDIR/$SCRIPTDIR/logs
+WORKDIR=$HOMEDIR/$SCRIPTDIR/work
+PREVIOUSSTAKEFILE=$WORKDIR/stake.previous.value
+CURRENTSTAKEFILE=$WORKDIR/stake.current.value
+PREVIOUSINFOFILE=$WORKDIR/getinfo.previous
+CURRENTINFOFILE=$WORKDIR/getinfo.current
+#
+######################
+# Code Execution Below
+#
+if ! test -d $PIDDIR
+then
+    mkdir $PIDDIR
+fi
+if ! test -s $PIDDIR/$SCRIPTNAME.pid
+then
+    echo 69696969 > $PIDDIR/$SCRIPTNAME.pid
+fi
+if ! test -d $LOGDIR
+then
+    mkdir $LOGDIR
+fi
+if ! test -d $WORKDIR
+then
+    mkdir $WORKDIR
+fi
+#
+###################################################
+# Check to see if I am already running, if so, exit
+#
+CHECKPID=$(<$PIDDIR/$SCRIPTNAME.pid)
+if ps -ef |grep -w $CHECKPID |grep $SCRIPTNAME > /dev/null
+then
+    exit 1
+else
+    ########################
+    # Main Program Execution
+    #
+    # First, write current Process ID to .pid file
+    #
+    echo $$ > $PIDDIR/$SCRIPTNAME.pid
+    #
+    # If previous stake file does not exist, switch to TEST NOTIFICATION
+    #
+    if ! test -s $PREVIOUSSTAKEFILE
+    then
+        SUBJECT="QTUM Notify Test"
+        echo "Test" > $PREVIOUSSTAKEFILE
+        echo "Test_Email" > $PREVIOUSINFOFILE
+    fi
+    #
+    # Get info from QTUM wallet
+    #
+    $QTUMCLI getinfo > $CURRENTINFOFILE
+    #
+    # Check if wallet is unlocked - if not, send notifications
+    #
+    UNLOCKED=`grep "unlocked_until" $CURRENTINFOFILE |cut -d ':' -f2 |sed 's/ //g' |sed 's/,//g'`
+    #
+    if [ $UNLOCKED -eq 0 ]
+    then
+        # If we have already sent wallet-locked notifications, exit
+        #
+        if test -s $WORKDIR/locked_notify
+        then
+            exit
+        else
+            # Email wallet locked notifications
+            #
+            SUBJECT="QTUM Wallet is Locked"
+            echo -e "FROM: $SENDER\nTO: $NOTIFYRECIPIENT\nSubject: $SUBJECT\n\n\n\nQTUM Wallet is Locked!\n\nPlease log in and unlock it." | /usr/sbin/sendmail -t
+            echo -e "FROM: $SENDER\nTO: $INFORECIPIENT\nSubject: $SUBJECT\n\nQTUM Wallet is Locked!\n\nPlease log in and unlock it." | /usr/sbin/sendmail -t
+            #
+            # Create locked_notify flag
+            #
+            echo 1 > $WORKDIR/locked_notify
+            exit
+        fi
+    else
+        # if locked_notify flag exists, delete it
+        #
+        if test -s $WORKDIR/locked_notify
+        then
+            rm $WORKDIR/locked_notify
+        fi
+    fi
+    #
+    # Get bare stake values
+    #
+    cat $CURRENTINFOFILE |grep "\"stake\"\:" |cut -d ':' -f2 |sed 's/ //g' |sed 's/,//g' > $CURRENTSTAKEFILE
+    #
+    CURRENTSTAKE=$(<$CURRENTSTAKEFILE)
+    PREVIOUSSTAKE=$(<$PREVIOUSSTAKEFILE)
+    #
+    if [ "$CURRENTSTAKE" != "$PREVIOUSSTAKE" ]
+    then
+        #
+        # Detected a stake change - Wait for 6 confirmations (blocks)
+        #
+        BLOCK=`grep "blocks" $CURRENTINFOFILE |cut -d ':' -f2 |sed 's/ //g' |sed 's/,//g'`
+        CURRENTBLOCK=$BLOCK
+        TARGETBLOCK=$(( $CURRENTBLOCK + 6 ))
+        #
+        # If SUBJECT="QTUM Notify Test", then skipt the wait
+        #
+        if [ "$SUBJECT" == "QTUM Notify Test" ]
+        then
+            TARGETBLOCK=$CURRENTBLOCK
+        fi
+        #
+        until [ $CURRENTBLOCK -ge $TARGETBLOCK ]; do
+            #
+            sleep 10
+            LATESTSTAKE=`$QTUMCLI getinfo |grep "\"stake\"\:" |cut -d ':' -f2 |sed 's/ //g' |sed 's/,//g'`
+            #
+            # If latest stake value equals previous stake value,
+            # then log orphaned stake and exit
+            #
+            if [ $LATESTSTAKE == $PREVIOUSSTAKE ]
+            then
+                DATE=`date`
+                echo "$DATE,Block=$BLOCK" >> $LOGDIR/stake_orphans.log
+                exit
+            fi
+            #
+            CURRENTBLOCK=`$QTUMCLI getinfo |grep "blocks" |cut -d ':' -f2 |sed 's/ //g' |sed 's/,//g'`
+        done
+        #
+        # 6 confirmations have completed - stake change is valid
+        #
+        # If SUBJECT is not "QTUM Notify Test", log the stake change
+        #
+        if [ "$SUBJECT" != "QTUM Notify Test" ]
+        then
+            DATE=`date`
+            echo "$DATE,Block=$BLOCK,$PREVIOUSSTAKE,$CURRENTSTAKE" >> $LOGDIR/stake_changes.log
+        fi
+        #
+        # Email stake change notifications
+        #
+        CURRENTINFO=$(<$CURRENTINFOFILE)
+        PREVIOUSINFO=$(<$PREVIOUSINFOFILE)
+        #
+        echo -e "FROM: $SENDER\nTO: $NOTIFYRECIPIENT\nSubject: $SUBJECT\n\n\n\nCurrent stake: $CURRENTSTAKE\n\nPrevious stake: $PREVIOUSSTAKE\n\nCheck email for more information..." | /usr/sbin/sendmail -t
+        echo -e "FROM: $SENDER\nTO: $INFORECIPIENT\nSubject: $SUBJECT\n\nQTUM Stake Change...\n\nCurrent getinfo:\n\n$CURRENTINFO\n\nPrevious getinfo:\n\n$PREVIOUSINFO" | /usr/sbin/sendmail -t
+        #
+        mv $CURRENTSTAKEFILE $PREVIOUSSTAKEFILE
+        mv $CURRENTINFOFILE $PREVIOUSINFOFILE
+    else
+        rm $CURRENTINFOFILE
+        rm $CURRENTSTAKEFILE
+    fi
+    #
+    exit
+fi
